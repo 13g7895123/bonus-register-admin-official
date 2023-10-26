9dc75ead55079c42799c32fbc0d2ad50bfbff7f6
yu xian <13g7895123@gmail.com>
2023-10-26 14:26:13 +0800

    [edit] fix variant same name error

--- vue-app/src/config/common.js
+++ vue-app/src/config/common.js
@@ -1,3 +1,3 @@
 export const domain = 'https://login.pcgame.tw/'
 export const img_domain = 'http://missa.mercylife.cc/'
-export const apiUrl = 'http://apix.mercylife.cc/api/bonus-register/admin/'
+export const api_url = 'http://apix.mercylife.cc/api/bonus-register/admin/'
