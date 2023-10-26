c1c1182a7405cb75a265fd70eb9d2cb82a9d2641
yu xian <13g7895123@gmail.com>
2023-10-26 14:40:54 +0800

    [edit] remove base param

--- vue-app/vite.config.js
+++ vue-app/vite.config.js
@@ -6,7 +6,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './admin/',
+  // base: './admin/',
   plugins: [
     vue(),
     AutoImport({
