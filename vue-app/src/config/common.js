0f3d897b0bdd96b7be7ef8a48653537bc5844272
Jarvis <13g7895123@gmail.com>
2023-10-26 22:07:08 +0800

    [edit] revise api path to https use

--- vue-app/src/config/common.js
+++ vue-app/src/config/common.js
@@ -1,3 +1,4 @@
 export const domain = 'https://login.pcgame.tw/'
 export const img_domain = 'http://missa.mercylife.cc/'
-export const api_url = 'http://apix.mercylife.cc/api/bonus-register/admin/'
+// export const api_url = 'http://apix.mercylife.cc/api/bonus-register/admin/'
+export const api_url = 'https://api.mercylife.cc/api/bonus-register/admin/'
