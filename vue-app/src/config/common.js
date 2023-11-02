526d84ea4e0d8e8c08365c9fbf7cb6b04e7642b0
Jarvis <13g7895123@gmail.com>
2023-11-02 23:39:56 +0800

    [edit] revise api url

--- vue-app/src/config/common.js
+++ vue-app/src/config/common.js
@@ -1,4 +1,5 @@
 export const domain = 'https://login.pcgame.tw/'
-export const img_domain = 'http://missa.mercylife.cc/'
+// export const img_domain = 'http://missa.mercylife.cc/'
 // export const api_url = 'http://apix.mercylife.cc/api/bonus-register/admin/'
-export const api_url = 'https://api.mercylife.cc/api/bonus-register/admin/'
+// export const api_url = 'https://api.mercylife.cc/api/bonus-register/admin/'
+export const api_url = 'https://127.0.0.1/api/main/admin/'
